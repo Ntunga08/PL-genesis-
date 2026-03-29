bdcee4c47901be2e799fa4530fed6bcde3f04e41
Ntunga08 <samwelbuluba22@gmail.com>
2026-03-29 16:23:12 +0300

    Improve constants file documentation

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,5 @@
-// Contract deployed on sepolia
+// HealthLink Smart Contract Configuration
+// Contract deployed on Ethereum Sepolia Testnet
 export const CONTRACT_ADDRESS = "0x97A985354D340306633670DB945A736A57102a00";
 
 // Chain ID for sepolia
